ae96dc5737601b920162abeebd82ab0838eebc8d
SenanB16 <50134624+SenanB16@users.noreply.github.com>
2022-08-30 21:35:23 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,4 @@
+let play = 0
 let score = 0
 input.onButtonPressed(Button.A, function () {
     basic.showLeds(`
@@ -7,21 +8,52 @@ input.onButtonPressed(Button.A, function () {
         . # # # .
         . . . . .
         `)
-    score += randint(0, 1)
+    play = randint(0, 1)
+    if (play == 0) {
+        basic.showIcon(IconNames.Square)
+    } else {
+        basic.showIcon(IconNames.Scissors)
+        score += 1
+    }
 })
 input.onGesture(Gesture.ScreenDown, function () {
     basic.showIcon(IconNames.Square)
-    score += randint(0, 1)
+    play = randint(0, 1)
+    if (play == 0) {
+        basic.showIcon(IconNames.Scissors)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+        score += 1
+    }
 })
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(score)
-    if (score < 3) {
+    if (score == 3) {
         basic.showString("Win!")
     } else {
         basic.showString("Lose!")
     }
+    score = 0
 })
 input.onGesture(Gesture.Shake, function () {
     basic.showIcon(IconNames.Scissors)
-    score += randint(0, 1)
+    play = randint(0, 1)
+    if (play == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . # # .
+            . # # # .
+            . # # # .
+            . . . . .
+            `)
+    } else {
+        basic.showIcon(IconNames.Square)
+        score += 1
+    }
 })
